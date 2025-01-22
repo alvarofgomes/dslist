ac24e5709823aadc60b0a28add2f1e42692313ab
Alvaro Gomes <alvarogomes098@gmail.com>
2025-01-22 12:44:48 -0300

    adc pesquisa por ordem de listas

--- src/main/java/com/alvarofgomes/dslist/dto/GameMinDTO.java
+++ src/main/java/com/alvarofgomes/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.alvarofgomes.dslist.dto;
 
 import com.alvarofgomes.dslist.entities.Game;
+import com.alvarofgomes.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -19,6 +20,14 @@ public class GameMinDTO {
 		year = entity.getYear();
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
+	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
 	}
 
 	public Long getId() {
